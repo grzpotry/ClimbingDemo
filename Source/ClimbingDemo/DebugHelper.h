e70fe089f2d0aae640fa683a675e2813ffe3dd93
grzpotry <grzpotry@gmail.com>
2023-08-15 22:19:07 +0200

    added DebugHelper

---
+++ Source/ClimbingDemo/DebugHelper.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+namespace Debug
+{
+	static void Print(const FString& Msg, const FColor& Color = FColor::MakeRandomColor(), int32 key = -1)
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(key, 6.0f, Color, Msg);
+		}
+
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *Msg);
+	}
+}
