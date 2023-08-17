1e957304a5491626cef2637caef23a02acca4efe
grzpotry <grzpotry@gmail.com>
2023-08-17 22:52:53 +0200

    added capsule & line trace

--- Source/ClimbingDemo/Private/Components/CustomMovementComponent.cpp
+++ Source/ClimbingDemo/Private/Components/CustomMovementComponent.cpp
@@ -2,4 +2,71 @@
 
 
 #include "Components/CustomMovementComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "ClimbingDemo/ClimbingDemoCharacter.h"
 
+void UCustomMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+	FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	TraceClimbableSurfaces();
+	TraceFromEyeHeight(100.f);
+}
+
+TArray<FHitResult> UCustomMovementComponent::DoCapsuleTraceMultiByObject(const FVector& Start, const FVector& End,
+                                                                         bool bShowDebug)
+{
+	TArray<FHitResult> OutCapsuleTraceHitResults;
+
+	UKismetSystemLibrary::CapsuleTraceMultiForObjects(
+		this,
+		Start,
+		End,
+		ClimbCapsuleTraceRadius,
+		ClimbCapsuleTraceHalfHeight,
+		ClimbableSurfaceTraceTypes,
+		false,
+		TArray<AActor*>(),
+		bShowDebug ? EDrawDebugTrace::ForOneFrame : EDrawDebugTrace::None,
+		OutCapsuleTraceHitResults, false);
+
+	return OutCapsuleTraceHitResults;
+}
+
+FHitResult UCustomMovementComponent::DoLineTraceSingleByObject(const FVector& Start, const FVector& End, bool bShowDebug)
+{
+	FHitResult OutHit;
+
+	UKismetSystemLibrary::LineTraceSingleForObjects(
+		this,
+		Start,
+		End,
+		ClimbableSurfaceTraceTypes,
+		false,
+		TArray<AActor*>(),
+		bShowDebug ? EDrawDebugTrace::ForOneFrame : EDrawDebugTrace::None,
+		OutHit, false);
+
+	return OutHit;
+}
+
+void UCustomMovementComponent::TraceClimbableSurfaces()
+{
+	FVector forwardVector = UpdatedComponent->GetForwardVector();
+	FVector offset = forwardVector * 30.0f;
+	FVector start = UpdatedComponent->GetComponentLocation() + offset;
+	FVector end = start + forwardVector;
+
+	DoCapsuleTraceMultiByObject(start, end, true);
+}
+
+void UCustomMovementComponent::TraceFromEyeHeight(float TraceDistance, float TraceStartOffset)
+{
+	const FVector ComponentLocation = UpdatedComponent->GetComponentLocation();
+	const FVector EyeHeightOffset = UpdatedComponent->GetUpVector() * (CharacterOwner->BaseEyeHeight + TraceStartOffset);
+	const FVector Start = ComponentLocation + EyeHeightOffset;
+	const FVector End = Start + UpdatedComponent-> GetForwardVector() * TraceDistance;
+
+	DoLineTraceSingleByObject(Start, End, true);
+}
--- Source/ClimbingDemo/Public/Components/CustomMovementComponent.h
+++ Source/ClimbingDemo/Public/Components/CustomMovementComponent.h
@@ -13,5 +13,21 @@ UCLASS()
 class CLIMBINGDEMO_API UCustomMovementComponent : public UCharacterMovementComponent
 {
 	GENERATED_BODY()
+
+	void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	TArray<FHitResult> DoCapsuleTraceMultiByObject(const FVector& Start, const FVector& End, bool bShowDebug = false);
+	FHitResult DoLineTraceSingleByObject(const FVector& Start, const FVector& End, bool bShowDebug = false);
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category= "Character Movement: Climbing", meta = (AllowPrivateAccess = "true"))
+	TArray<TEnumAsByte<EObjectTypeQuery>> ClimbableSurfaceTraceTypes;
+
+	void TraceClimbableSurfaces();
+	void TraceFromEyeHeight(float TraceDistance, float TraceStartOffset = 0.f);
 	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category= "Character Movement: Climbing", meta = (AllowPrivateAccess = "true"))
+	float ClimbCapsuleTraceRadius = 50.f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category= "Character Movement: Climbing", meta = (AllowPrivateAccess = "true"))
+	float ClimbCapsuleTraceHalfHeight = 75.f;
 };
