43eeade01ba93629e7ff2e9474e00fdf5110d117
grzpotry <grzpotry@gmail.com>
2023-10-03 23:05:38 +0200

    stacktrace support

--- Source/ClimbingDemo/DebugHelper.h
+++ Source/ClimbingDemo/DebugHelper.h
@@ -2,13 +2,23 @@
 
 namespace Debug
 {
-	static void Print(const FString& Msg, const FColor& Color = FColor::MakeRandomColor(), int32 key = -1)
+	static void Print(const FString& Msg, const FColor& Color = FColor::MakeRandomColor(), int32 key = -1, bool printCallStack = false)
 	{
 		if (GEngine)
 		{
 			GEngine->AddOnScreenDebugMessage(key, 6.0f, Color, Msg);
 		}
 
+		if (printCallStack)
+		{
+			FDebug::DumpStackTraceToLog(ELogVerbosity::Warning);
+		}
+	
 		UE_LOG(LogTemp, Warning, TEXT("%s"), *Msg);
+	}
+
+	static void PrintWithStack(const FString& Msg, const FColor& Color = FColor::MakeRandomColor(), int32 key = -1)
+	{
+		Print(Msg, Color, key, true);
 	}
 }
