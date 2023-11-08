4dbd398fa4dda2ff770ca646388e219bd794f905
grzpotry <grzpotry@gmail.com>
2023-11-08 22:22:51 +0100

    custom animation blueprint

--- Source/ClimbingDemo/Private/CharacterAnimInstance.cpp
+++ Source/ClimbingDemo/Private/CharacterAnimInstance.cpp
@@ -3,3 +3,41 @@
 
 #include "CharacterAnimInstance.h"
 
+#include "Components/CustomMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+
+void UCharacterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	UpdateGroundSpeed();
+}
+
+void UCharacterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	Character = Cast<AClimbingDemoCharacter>(TryGetPawnOwner());
+
+	if (Character)
+	{
+		CustomMovement = Character->GetCustomMovementComponent();
+	}
+}
+
+void UCharacterAnimInstance::UpdateGroundSpeed()
+{
+	if (Character)
+	{
+		CustomMovement = Character->GetCustomMovementComponent();
+		GroundSpeed = UKismetMathLibrary::VSizeXY(Character->GetVelocity());
+		AirSpeed = Character->GetVelocity().Z;
+
+		if (CustomMovement)
+		{
+			IsFalling = CustomMovement->IsFalling();
+		}
+
+	}
+}
--- Source/ClimbingDemo/Public/CharacterAnimInstance.h
+++ Source/ClimbingDemo/Public/CharacterAnimInstance.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
+#include "ClimbingDemo/ClimbingDemoCharacter.h"
 #include "CharacterAnimInstance.generated.h"
 
 /**
@@ -14,4 +15,23 @@ class CLIMBINGDEMO_API UCharacterAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= "Reference", meta = (AllowPrivateAccess = "true"))
+	float GroundSpeed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= "Reference", meta = (AllowPrivateAccess = "true"))
+	float AirSpeed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= "Reference", meta = (AllowPrivateAccess = "true"))
+	bool IsFalling;
+
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	virtual void NativeInitializeAnimation() override;
+
+	UPROPERTY()
+	AClimbingDemoCharacter * Character;
+
+	UPROPERTY()
+	UCustomMovementComponent * CustomMovement;
+
+	void UpdateGroundSpeed();
 };
